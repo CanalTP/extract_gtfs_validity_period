0d6c5601c1e1f0b3082b2f172a2deea17d5c5e83
Noémie Lehuby <noemie.lehuby@openmailbox.org>
2016-12-29 11:54:36 +0100

    also use calendar_dates.txt

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,13 @@ struct Calendar {
     end_date: u32,
 }
 
+#[derive(RustcDecodable, Debug)]
+struct CalendarDate {
+    service_id: String,
+    date: u32,
+    exception_type: u32,
+}
+
 fn main() {
     let path = "calendar.txt";
     let mut reader =  csv::Reader::from_file(path).unwrap().has_headers(true);
@@ -28,9 +35,24 @@ fn main() {
         end_dates.push(a_calendar.end_date);
     }
 
+    let path = "calendar_dates.txt";
+    let mut reader =  csv::Reader::from_file(path).unwrap().has_headers(true);
+
+    for row in reader.decode() {
+        let a_calendar_date: CalendarDate = row.unwrap();
+
+        //just like Google FeedValidator, we enlarge the validity period with exception dates added
+        // but we do not shrink it with exception dates removed.
+        // https://github.com/google/transitfeed/blob/master/transitfeed/serviceperiod.py#L80
+        if a_calendar_date.exception_type == 1 {
+            end_dates.push(a_calendar_date.date);
+        }
+
+    }
+
     let calendar_min_start_date: &u32 = start_dates.iter().min().unwrap();
     let calendar_max_end_date: &u32 = end_dates.iter().max().unwrap();
 
-    println!("début (calendar): {}", calendar_min_start_date );
-    println!("fin (calendar) : {}", calendar_max_end_date );
+    println!("start : {}", calendar_min_start_date );
+    println!("end : {}", calendar_max_end_date );
 }
